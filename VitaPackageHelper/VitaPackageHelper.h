807467ebffe77e37241da3a42da2e613ee08cd0d
HexPang <hexpang@jabstruse.com>
2016-09-02 20:24:23 +0800

    Update

--- VitaPackageHelper/VitaPackageHelper.h
+++ VitaPackageHelper/VitaPackageHelper.h
@@ -9,5 +9,5 @@
 #import <Foundation/Foundation.h>
 
 @interface VitaPackageHelper : NSObject
-
+- (NSArray *) loadPackage:(NSString*)path;
 @end
--- VitaPackageHelper/VitaPackageHelper.m
+++ VitaPackageHelper/VitaPackageHelper.m
@@ -10,9 +10,35 @@
 
 @implementation VitaPackageHelper
 
-- (void) loadPackage:(NSString*)path{
+- (NSArray *) loadPackage:(NSString*)path{
+    NSMutableArray *packages = [[NSMutableArray alloc]init];
     NSFileManager *fm = [[NSFileManager alloc]init];
+    NSError *err;
+    NSArray *files = [fm contentsOfDirectoryAtURL:[NSURL URLWithString:path] includingPropertiesForKeys:nil options:0 error:&err];
+    for (NSURL *fileUrl in files) {
+        NSString *file = [fileUrl absoluteString];
+        NSString *ext = [[file pathExtension] lowercaseString];
+        BOOL isDir = NO;
     
+        BOOL ahaha = [fm fileExistsAtPath:file isDirectory:&isDir];
+        if(isDir){
+            //目录处理，继续便利!
+            NSLog(@"IS DIR %@",file);
+            NSArray *packs = (NSArray *)[self loadPackage:file];
+            NSLog(@"AH:%@",packs);
+            [packages addObjectsFromArray:packs];
+        }
+        if([ext isEqualToString:@"vpk"]){
+            [packages addObject:file];
+        }
+    }
+    
+    return packages;
+//    NSLog(@"%@",files);
 }
+//
+//- (NSDictionary *) loadSFO:(NSString*) package{
+//    
+//}
 
 @end
